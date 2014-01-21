b82b4c01442b002e711738f0bc772019db8d3553
Andreas Öman <andreas@lonelycoder.com>
2014-01-21 05:35:40 +0100

    Fix talloc leaks

--- talloc.c
+++ talloc.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 
@@ -7,22 +8,41 @@ typedef struct talloc_item {
   struct talloc_item *next;
 } talloc_item_t;
 
-static talloc_item_t __thread *talloc_queue;
+//static talloc_item_t __thread *talloc_queue;
 static pthread_key_t talloc_key;
+
 
 
 /**
  *
  */
-void
-talloc_cleanup(void)
+static talloc_item_t **
+talloc_getq(void)
+{
+  talloc_item_t **q = pthread_getspecific(talloc_key);
+  if(q)
+    return q;
+
+  q = calloc(1, sizeof(talloc_item_t *));
+  printf("Created talloc queue for thread %lx\n", pthread_self());
+  pthread_setspecific(talloc_key, q);
+  return q;
+}
+
+
+
+/**
+ *
+ */
+static void
+talloc_free_items(talloc_item_t **q)
 {
   talloc_item_t *p, *next;
-  for(p = talloc_queue; p != NULL; p = next) {
+  for(p = *q; p != NULL; p = next) {
     next = p->next;
     free(p);
+    printf("Free'd %p\n", p);
   }
-  talloc_queue = NULL;
 }
 
 
@@ -32,7 +52,33 @@ talloc_cleanup(void)
 static void
 talloc_thread_cleanup(void *aux)
 {
-  talloc_cleanup();
+  talloc_free_items(aux);
+  free(aux);
+}
+
+
+/**
+ *
+ */
+void
+talloc_cleanup(void)
+{
+  talloc_item_t **q = pthread_getspecific(talloc_key);
+  if(q != NULL)
+    talloc_free_items(q);
+}
+
+
+/**
+ *
+ */
+static void
+talloc_insert(talloc_item_t *t)
+{
+  talloc_item_t **q = talloc_getq();
+  t->next = *q;
+  *q = t;
+  printf("Alloced %p on %lx\n", t, pthread_self());
 }
 
 
@@ -43,8 +89,7 @@ void *
 talloc_malloc(size_t s)
 {
   talloc_item_t *t = malloc(s + sizeof(talloc_item_t));
-  t->next = talloc_queue;
-  talloc_queue = t;
+  talloc_insert(t);
   return t + 1;
 }
 
@@ -56,8 +101,7 @@ void *
 talloc_zalloc(size_t s)
 {
   talloc_item_t *t = calloc(1, s + sizeof(talloc_item_t));
-  t->next = talloc_queue;
-  talloc_queue = t;
+  talloc_insert(t);
   return t + 1;
 }
 
