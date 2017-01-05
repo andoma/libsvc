1834ca0f767e1e6541c91800c5995f17c1578f27
Andreas Smas <andreas@lonelycoder.com>
2017-01-05 14:14:53 +0100

    misc: Add _GNU_SOURCE for asprintf

--- misc.c
+++ misc.c
@@ -20,7 +20,7 @@
 * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ******************************************************************************/
-
+#define _GNU_SOURCE
 #include <assert.h>
 #include <sys/types.h>
 #include <sys/stat.h>
