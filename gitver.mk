6b0d9fac87ef76f301fbf23d13777e5147763264
Andreas Smas <andreas@lonelycoder.com>
2019-04-11 17:20:49 -0700

    Make GIT_DESCRIBE_OUTPUT variable conditional

--- gitver.mk
+++ gitver.mk
@@ -7,7 +7,7 @@ endif
 
 GITVER_VARGUARD = $(1)_GUARD_$(shell echo $($(1)) | ${GITVER_MD5} | cut -d ' ' -f 1)
 
-GIT_DESCRIBE_OUTPUT := $(shell git describe --always --dirty)
+GIT_DESCRIBE_OUTPUT ?= $(shell git describe --always --dirty)
 
 ${BUILDDIR}/version_git.h: ${BUILDDIR}/gitver/$(call GITVER_VARGUARD,GIT_DESCRIBE_OUTPUT)
 	echo >$@ "#define VERSION_GIT \"${GIT_DESCRIBE_OUTPUT}\""
